aa45dd577dbaa207ee3cbb641a75837b2b3c9102
callumthomas086 <callumthomas086@gmail.com>
2021-12-06 13:16:40 +0000

    .

--- Code/Logic.py
+++ Code/Logic.py
@@ -1,47 +1,135 @@
-
-# Logic file to be imported into the main file. This will allow the game to function and will later be used train the AI.
-
-
-# Imports
-
 import random
-import time
+import Constants as c
 
-# create a function to initialize the game
 
-def start_game():
-    # declare an empty lsit then appending 4 lists each with 4 elements
 
+
+def new_game(n):
+    """
+    This function starts a new game.
+    """
     matrix = []
-
-    for i in range (4):
-        matrix.append([0] * 4)
-
-    # print the controls for the user 
-
-
-    print ("the commands are as follows : ")
-    print ("up arrow key - moves the tiles up")
-    print ("down arrow key - moves the tiles down")
-    print ("left arrow key - moves the tiles left")
-    print ("right arrow key - moves the tiles right")
-
-
-    add_new_tile(matrix)
+    for i in range(n):
+        matrix.append([0] * n)
+    matrix = add_two(matrix)
+    matrix = add_two(matrix)
     return matrix
 
-
-def add_new_tile(matrix):
-    # function to add a new tile to a random empty space in the matrix
-
-
-    r = random.randint(0, 3)
-    c = random.randint(0, 3)
-
-
-    while (matrix[r] != 0):
-        r = random.randint(0, 3)
-        c = random.randint(0, 3)
+def add_two(mat):
+    a = random.randint(0, len(mat) - 1)
+    b = random.randint(0, len(mat) - 1)
+    while mat[a][b] != 0:
+        a = random.randint(0, len(mat) - 1)
+        b = random.randint(0, len(mat) - 1)
+    mat[a][b] = 2
+    return mat
 
 
-    matrix[r] = 2  
+
+def game_state(mat):
+    """
+    This function checks if the game is over.
+    """
+    for i in range(len(mat)):
+        for j in range(len(mat)):
+            if mat[i][j] == 2048:
+                return 'win'
+    for i in range(len(mat)):
+        for j in range(len(mat)):
+            if mat[i][j] == 0:
+                return 'not over'
+    for i in range(len(mat) -1):
+        for j in range(len(mat) -1):
+            if mat[i][j] == mat[i][j+1] or mat[i][j] == mat[i+1][j]:
+                return 'not over'
+    for k in range(len(mat)-1):
+        if mat[len(mat)-1][k] == mat[len(mat)-1][k+1]:
+            return 'not over'
+    for j in range(len(mat)-1):
+        if mat[j][len(mat)-1] == mat[j+1][len(mat)-1]:
+            return 'not over'
+    return 'lose'
+
+
+def reverse(mat):
+    new = []
+    for i in range(len(mat)):
+        new.append([])
+        for j in range(len(mat[0])):
+            new[i].append(mat[i][len(mat[0]) - j - 1])
+    return new
+
+def transpose(mat):
+    new = []
+    for i in range(len(mat[0])):
+        new.append([])
+        for j in range(len(mat)):
+            new[i].append(mat[j][i])
+    return new
+
+
+
+def cover_up(mat):
+    new = []
+    for j in range(c.GRID_LEN):
+        partial_new = []
+        for i in range(c.GRID_LEN):
+            partial_new.append(0)
+        new.append(partial_new)
+    done = False
+    for i in range(c.GRID_LEN):
+        count = 0
+        for j in range(c.GRID_LEN):
+            if mat[i][j] != 0:
+                new[i][count] = mat[i][j]
+                if j != count:
+                    done = True
+                count += 1
+    return new, done
+
+def merge(mat, done):
+    for i in range(c.GRID_LEN):
+        for j in range(c.GRID_LEN-1):
+            if mat[i][j] == mat[i][j+1] and mat[i][j] != 0:
+                mat[i][j] *= 2
+                mat[i][j+1] = 0
+                done = True
+    return mat, done
+
+def up(game):
+    print("up")
+    # return matrix after shifting up
+    game = transpose(game)
+    game, done = cover_up(game)
+    game, done = merge(game, done)
+    game = cover_up(game)[0]
+    game = transpose(game)
+    return game, done
+
+def down(game):
+    print("down")
+    # return matrix after shifting down
+    game = reverse(transpose(game))
+    game, done = cover_up(game)
+    game, done = merge(game, done)
+    game = cover_up(game)[0]
+    game = transpose(reverse(game))
+    return game, done
+
+def left(game):
+    print("left")
+    # return matrix after shifting left
+    game, done = cover_up(game)
+    game, done = merge(game, done)
+    game = cover_up(game)[0]
+    return game, done
+
+def right(game):
+    print("right")
+    # return matrix after shifting right
+    game = reverse(game)
+    game, done = cover_up(game)
+    game, done = merge(game, done)
+    game = cover_up(game)[0]
+    game = reverse(game)
+    return game, done
